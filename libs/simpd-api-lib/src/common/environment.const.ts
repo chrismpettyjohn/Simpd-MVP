7bc83f764eb557edbec64e6cfc16d3c3e75dd6f6
chrismpettyjohn <chrismpettyjohn@gmail.com>
2023-04-23 21:45:16 -0400

    Clean up environment file

--- libs/simpd-api-lib/src/common/environment.const.ts
+++ libs/simpd-api-lib/src/common/environment.const.ts
@@ -11,34 +11,25 @@ export const ROLE_SERVICE_PORT = 3001;
 export const USER_SERVICE_PORT = 3002;
 export const SESSION_SERVICE_PORT = 3003;
 
+const SERVICE_NAME_PREFIX = 'simpd.';
+
 export const USER_SERVICE_NAME = 'simpd.user';
 export const ROLE_SERVICE_NAME = 'simpd.role';
 export const SESSION_SERVICE_NAME = 'simpd.session';
 
-export const USER_SERVICE_PROTO = join(
-  __dirname,
-  '..',
-  '..',
-  '..',
-  '..',
-  'proto',
-  'simpd-api-user/user.proto'
-);
-export const ROLE_SERVICE_PROTO = join(
-  __dirname,
-  '..',
-  '..',
-  '..',
-  '..',
-  'proto',
-  'simpd-api-role/role.proto'
-);
-export const SESSION_SERVICE_PROTO = join(
-  __dirname,
-  '..',
-  '..',
-  '..',
-  '..',
-  'proto',
-  'simpd-api-session/session.proto'
-);
+function resolveProto(serviceName: string): string {
+  const serviceNameWithoutPrefix = serviceName.replace(SERVICE_NAME_PREFIX, '');
+  return join(
+    __dirname,
+    '..',
+    '..',
+    '..',
+    '..',
+    'proto',
+    `simpd-api-${serviceNameWithoutPrefix}/${serviceNameWithoutPrefix}.proto`
+  );
+}
+
+export const USER_SERVICE_PROTO = resolveProto(USER_SERVICE_NAME);
+export const ROLE_SERVICE_PROTO = resolveProto(ROLE_SERVICE_NAME);
+export const SESSION_SERVICE_PROTO = resolveProto(SESSION_SERVICE_NAME);
