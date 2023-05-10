529172d9e0c77543f3420242566128743c356cfe
chrismpettyjohn <chrismpettyjohn@gmail.com>
2023-05-10 01:57:14 -0400

    pm2 restarts for lib changes

--- services/svc-gateway/src/index.ts
+++ services/svc-gateway/src/index.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
-import { NestFactory } from '@nestjs/core';
-import { GatewayModule } from './gateway.module';
+import {NestFactory} from '@nestjs/core';
+import {GatewayModule} from './gateway.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
