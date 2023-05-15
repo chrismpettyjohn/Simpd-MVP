070a9fa95beaddd134b1ef541e47137f37d23634
chrismpettyjohn <chrismpettyjohn@gmail.com>
2023-05-15 02:47:07 -0400

    Fix profile editort

--- apps/app-simpd/src/screens/settings-profile/profile-settings-card/ProfileSettingsCard.tsx
+++ apps/app-simpd/src/screens/settings-profile/profile-settings-card/ProfileSettingsCard.tsx
@@ -1,28 +1,27 @@
-import { Button } from 'components/button/Button';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { ProfileEditor } from 'components/profile-editor/ProfileEditor';
 import { CardAccordion } from 'components/card-accordion/CardAccordion';
-import { ProfileUpdateInput, sessionContext, useProfileUpdate } from '@simpd/lib-web';
+import { ProfileUpdateInput, sessionContext, useProfileFetchOne, useProfileUpdate } from '@simpd/lib-web';
 
 export function ProfileSettingsCard() {
-  const { profile } = useContext(sessionContext);
+  const { session } = useContext(sessionContext);
   const updateProfile = useProfileUpdate();
+  const fetchProfile = useProfileFetchOne(({ id: session.profileID }));
+
+  useEffect(() => {
+    fetchProfile.fetch();
+  }, [session.profileID])
 
   const onUpateProfile = async (profileDTO: ProfileUpdateInput) => {
-    await updateProfile.execute({ profileID: profile?.id, username: profile?.username, changes: profileDTO });
+    await updateProfile.execute({ profileID: session.profileID, changes: profileDTO });
   }
 
 
   return (
-    <CardAccordion defaultIsOpen header="Profile">
+    <CardAccordion header="Profile">
       {
-        profile && (
-          <ProfileEditor defaultProfile={profile} onSave={onUpateProfile} />
-        )
-      }
-      {
-        !profile && (
-          <p>Select a profile to get started</p>
+        fetchProfile.data && (
+          <ProfileEditor defaultProfile={fetchProfile.data} onSave={onUpateProfile} />
         )
       }
     </CardAccordion>
