5dfbd0970a62859f1687e23ae52cc5a2e119ffc4
chrismpettyjohn <chrismpettyjohn@gmail.com>
2023-05-23 03:34:09 -0400

    Cleanup

--- apps/app-simpd/src/components/author-block-large/AuthorBlockLarge.tsx
+++ apps/app-simpd/src/components/author-block-large/AuthorBlockLarge.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import { Link } from 'wouter';
 import { AuthorBlockLargeProps } from './AuthorBlockLarge.types';
+
 import { AuthorBlockLargeElement, AuthorBlockUserInfoContainer } from './AuthorBlockLarge.sty';
 
 export function AuthorBlockLarge({ profile }: AuthorBlockLargeProps) {
   return (
-    <Link to={`/profiles/${profile.username}`}>
-      <AuthorBlockLargeElement>
-        <img
-          src={profile.profilePicture?.url}
-          className="post-card-image1"
-        />
-        <AuthorBlockUserInfoContainer>
+    <>
+      <Link to={`/profiles/${profile.username}`}>
+        <AuthorBlockLargeElement>
+          <img
+            src={profile.profilePicture?.url}
+            className="post-card-image1"
+          />
+        </AuthorBlockLargeElement>
+      </Link>
+      <AuthorBlockUserInfoContainer>
+        <Link to={`/profiles/${profile.username}`}>
           <h1>
             {profile.displayName}
           </h1>
-          <span>
-            @{profile.username}
-          </span>
-        </AuthorBlockUserInfoContainer>
-      </AuthorBlockLargeElement>
-    </Link>
+        </Link >
+        <Link to={`/profiles/${profile.username}`}></Link>
+        <span>
+          @{profile.username}
+        </span>
+      </AuthorBlockUserInfoContainer >
+    </>
   )
 }
--- apps/app-simpd/src/components/message-card/MessageCard.tsx
+++ apps/app-simpd/src/components/message-card/MessageCard.tsx
@@ -1,24 +1,20 @@
 import React from 'react'
-import { Link } from 'wouter';
 import { MessageCardProps } from './MessageCard.types';
 import { AuthorBlockSmall } from '../author-block-small/AuthorBlockSmall';
-import { MessageCardElement, MessageCardTextContainer, MessageIconElement } from './MessageCard.sty';
+import { MessageCardElement, MessageCardTextContainer } from './MessageCard.sty';
 
 export function MessageCard({ message, profile }: MessageCardProps) {
   return (
-    <Link to={`/messages/threads/${profile.username}`}>
-      <MessageCardElement>
-        <AuthorBlockSmall profile={profile} />
-        <MessageCardTextContainer>
-          <h1 className="author-container">
-            They Said:
-          </h1>
-          <span className="message-content">
-            {message.content}
-          </span>
-        </MessageCardTextContainer>
-        <MessageIconElement className="fa fa-share" />
-      </MessageCardElement>
-    </Link>
+    <MessageCardElement>
+      <AuthorBlockSmall profile={profile} />
+      <MessageCardTextContainer>
+        <h1 className="author-container">
+          They Said:
+        </h1>
+        <span className="message-content">
+          {message.content}
+        </span>
+      </MessageCardTextContainer>
+    </MessageCardElement>
   )
 }
