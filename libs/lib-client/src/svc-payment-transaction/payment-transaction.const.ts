48316496ed33d40a145af8867738f35a89968af4
chrismpettyjohn <chrismpettyjohn@gmail.com>
2023-05-29 02:16:35 -0400

    fix conflicts

--- libs/lib-client/src/svc-payment-transaction/payment-transaction.const.ts
+++ libs/lib-client/src/svc-payment-transaction/payment-transaction.const.ts
@@ -1,5 +1,5 @@
 export const SVC_PAYMENT_TRANSACTION_NAME = 'PaymentTransactionService';
-export const SVC_PAYMENT_TRANSACTION_WEB_PORT = 3048;
+export const SVC_PAYMENT_TRANSACTION_WEB_PORT = 3050;
 export const SVC_PAYMENT_TRANSACTION_WEB_ADDRESS =
   process.env.SVC_PAYMENT_TRANSACTION_WEB_ADDRESS ??
   `http://localhost:${SVC_PAYMENT_TRANSACTION_WEB_PORT}/graphql`;
--- services/svc-payment-transaction/src/web-server.ts
+++ services/svc-payment-transaction/src/web-server.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import {bootstrapDynamicService} from '@simpd/lib-api';
 import {PaymentInvoiceModule} from './payment-invoice.module';
-import {SVC_PAYMENT_INVOICE_WEB_PORT} from '@simpd/lib-client';
+import {SVC_PAYMENT_TRANSACTION_WEB_PORT} from '@simpd/lib-client';
 
-bootstrapDynamicService(PaymentInvoiceModule, SVC_PAYMENT_INVOICE_WEB_PORT);
+bootstrapDynamicService(PaymentInvoiceModule, SVC_PAYMENT_TRANSACTION_WEB_PORT);
