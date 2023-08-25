03e07b1b3a017ce0c9f144fae2ed94617a4ddc5b
chrismpettyjohn <chrismpettyjohn@gmail.com>
2023-08-25 00:13:23 -0400

    enhance: Add more info to beta card

--- apps/app-simpd/src/components/bug-testing-notice/BugTestingNotice.tsx
+++ apps/app-simpd/src/components/bug-testing-notice/BugTestingNotice.tsx
@@ -4,6 +4,9 @@ import { BugTestingNoticeCard } from './BugTestingNotice.styled';
 export function BugTestingNotice() {
   return (
     <BugTestingNoticeCard>
+      <b>CONFIDENTIAL</b>
+      <p> ACCESS IS ONLY GRANTED TO AFFILIATES OF SIMPD LLC WITH WRITTEN APPROVAL</p>
+      <br />
       <b>Beta Testing</b>
       <p>You may experience issues.  Please report all feedback <a href="https://docs.google.com/forms/d/e/1FAIpQLSeHkx_QnipKpjAAIjWKX53zOkEs50bnoECvA13VvEPr33XOow/viewform" target="_blank" rel="noreferrer">here</a></p>
     </BugTestingNoticeCard>
