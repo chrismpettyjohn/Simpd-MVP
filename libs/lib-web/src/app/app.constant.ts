30e7afc6127567d9f706478398473d0a73db9db0
chrismpettyjohn <chrismpettyjohn@gmail.com>
2023-08-24 23:17:28 -0400

    enhance: Store media api url in env

--- libs/lib-web/src/app/app.constant.ts
+++ libs/lib-web/src/app/app.constant.ts
@@ -1,5 +1,6 @@
 // @ts-ignore
 export const GRAPHQL_URL = import.meta.env.VITE_GRAPHQL_URL ?? 'http://localhost:3000/graphql'
+export const MEDIA_API_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:3012'
 
 export const LOCAL_STORAGE_SESSION_TOKEN = 'simpd.session';
 
--- libs/lib-web/src/hooks/use-media-upload.hook.ts
+++ libs/lib-web/src/hooks/use-media-upload.hook.ts
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import { LOCAL_STORAGE_SESSION_TOKEN } from "../app/app.constant";
+import { MEDIA_API_URL, LOCAL_STORAGE_SESSION_TOKEN } from "../app/app.constant";
 
 export interface useMediaUploadResponse {
   onUpload(file: File): Promise<number>;
   loading: boolean;
 }
 
-const MEDIA_API = 'http://localhost:3012';
+const MEDIA_API = `${MEDIA_API_URL}/media`
 
 export function useMediaUpload() {
   const [loading, setLoading] = useState(false);
@@ -22,7 +22,7 @@ export function useMediaUpload() {
       setLoading(true);
       const formData = new FormData();
       formData.append('file', file);
-      const response = await fetch(`${MEDIA_API}/media`, {
+      const response = await fetch(MEDIA_API, {
         headers: {
           Authorization: `Bearer ${bearerToken}`,
         },
