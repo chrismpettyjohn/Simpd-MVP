22c3abb81a5c8ed22e218bbaa1cab3dde019156d
chrismpettyjohn <chrismpettyjohn@gmail.com>
2023-12-17 19:55:42 -0500

    design: card changes

--- apps/app-simpd/src/components/card-accordion/CardAccordion.sty.ts
+++ apps/app-simpd/src/components/card-accordion/CardAccordion.sty.ts
@@ -3,6 +3,7 @@ import { styled } from "styled-components";
 export const CardAccordionHeaderContent = styled.div`
   align-items: center;
   cursor: pointer;
+  color: ${({ theme }) => theme.color.s90};
   display: flex;
   flex: 1;
   justify-content: space-between;
--- apps/app-simpd/src/components/card/Card.sty.ts
+++ apps/app-simpd/src/components/card/Card.sty.ts
@@ -3,7 +3,7 @@ import { styled } from "styled-components";
 export const CardContainer = styled.div`
   width: 100%;
   height: 100%;
-  border: 1px solid #F6DEDE;
+  border: ${({ theme }) => `1px solid ${theme.color.s50}`};
   border-radius: ${({ theme }) => theme.radius.oneUnit};
   padding: ${({ theme }) => theme.space.oneUnit};
 `
