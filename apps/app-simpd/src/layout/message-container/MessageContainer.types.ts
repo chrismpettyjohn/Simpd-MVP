52d3645eecbb2c31992a48f8c434bdcbcd287683
chrismpettyjohn <chrismpettyjohn@gmail.com>
2023-12-19 23:51:03 -0500

    cleanup

--- apps/app-simpd/src/layout/message-container/MessageContainer.types.ts
+++
@@ -1,7 +0,0 @@
-import { ProfileFragment } from "@simpd/lib-web";
-import { ReactNode } from "react";
-
-export interface MessageContainerProps {
-  children: ReactNode;
-  profile?: ProfileFragment;
-}
